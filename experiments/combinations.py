e1ff46c92b9b0f13921946d0f0f7cee71714bd56
Shroopd <dj@shroop.net>
2026-04-08 19:52:48 -0700

    Update combinations.py

--- experiments/combinations.py
+++ experiments/combinations.py
@@ -4,15 +4,16 @@ from itertools import product
 
 import numpy
 
-dims = 3
+dims = 4
+radius = 1
 
-foo = product((-1, 0, 1), repeat=dims)
+foo = product(range(-radius, radius + 1), repeat=dims)
 
 buckets = [[] for _ in range(dims + 1)]
 
 
 def dirs(X):
-    return dims-sum(i == 0 for i in X)
+    return dims - sum(1 if (i == 0) else 0 for i in X)
 
 
 for v in foo:
@@ -20,17 +21,20 @@ for v in foo:
 
 for bi in range(len(buckets)):
     b = buckets[bi]
-    b = list(1 + numpy.array(b).T)
+    b = list(radius + numpy.array(b).T)
     for i in range(len(b)):
         b[i] = tuple(b[i])
     buckets[bi] = tuple(b)  # type: ignore
 
 
-bar = numpy.zeros((3,) * dims, dtype=int)
+bar = numpy.zeros((radius * 2 + 1,) * dims, dtype=int)
 for d in range(dims):
-    numpy.swapaxes(bar, d, -1)[:] += numpy.arange(3) * 10 ** (dims - d - 1)
+    numpy.swapaxes(bar, d, -1)[:] += numpy.arange(radius * 2 + 1) * 10 ** (dims - d - 1)
 
 print(bar)
-for b in buckets:
+for i in range(len(buckets)):
+    b = buckets[i]
     # print(bar[tuple(1 + numpy.array(b).T)])
     print(bar[b])
+    bar[b] = i
+print(bar)
